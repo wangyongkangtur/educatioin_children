eee83b2588fb711d08948e2ce39b008273b1b7a1
wangyongkang <775781862@qq.com>
2019-01-01 13:06:57 +0800

    Initial Commit

---
+++ config.js
@@ -0,0 +1,28 @@
+/**
+ * 小程序配置文件
+ */
+
+// 此处主机域名是腾讯云解决方案分配的域名
+// 小程序后台服务解决方案：https://www.qcloud.com/solution/la
+
+const host = 'http://127.0.0.1'
+
+const config = {
+
+  // 下面的地址配合云端 Server 工作
+  host,
+
+  paperUrl: `https://${host}/paper`,
+
+  // paperPaginationUrl: `https://${host}/paper/pagination`,
+  paperPaginationUrl: `${host}/api/paper/pagination`,
+
+  questionUrl: `https://${host}/question`,
+
+  answerSheetUrl: `https://${host}/answer_sheet`,
+
+  userUrl: `https://${host}/user`,
+
+}
+
+module.exports = config
---
+++ pages/paper/paper.js
@@ -0,0 +1,95 @@
+// pages/paper/paper.js
+const paperPaginationUrl = require('../../config').paperPaginationUrl
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    paper: null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.request({
+      url: paperPaginationUrl,
+      data: {
+        "options": {
+          "limit": 10,
+          "skip": 1,
+          "sort": {
+            "date": "1"
+          }
+        }
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json' 
+      },
+      success(result) {
+        console.log('request success', result)
+        console.log('request success', result.errMsg)
+        console.log('request success', result.data)
+        that.setData({
+          paper: result.data.result.data
+        })
+      },
+      fail({ errMsg }) {
+        console.log('request fail', errMsg)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
